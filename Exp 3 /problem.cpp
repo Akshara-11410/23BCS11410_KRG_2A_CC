af0131dfd2cde9ea44e74b7256ce526b871b6d88
Akshara Chauhan <23BCS11410@cuchd.in>
2026-04-24 20:10:59 +0530

    Create problem.cpp

---
+++ Exp 3 /problem.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int nthMagicalNumber(int n, int a, int b) {
+        long long mod = 1e9 + 7;
+        long long l = 1, r = 1LL * n * min(a, b);
+        long long lcm = (1LL * a * b) / __gcd(a, b);
+
+        while (l < r) {
+            long long mid = (l + r) / 2;
+            long long count = mid / a + mid / b - mid / lcm;
+            if (count < n) l = mid + 1;
+            else r = mid;
+        }
+
+        return l % mod;
+    }
+};
