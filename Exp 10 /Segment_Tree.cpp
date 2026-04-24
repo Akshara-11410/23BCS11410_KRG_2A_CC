7be3967a7ec36d4aacf93eb90f47a28613cb8511
Akshara Chauhan <23BCS11410@cuchd.in>
2026-04-24 20:02:41 +0530

    Create Segment_Tree.cpp

---
+++ Exp 10 /Segment_Tree.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class SegmentTree {
+public:
+    int n;
+    vector<int> tree;
+
+    SegmentTree(vector<int>& nums) {
+        n = nums.size();
+        tree.resize(4 * n);
+        build(nums, 0, 0, n - 1);
+    }
+
+    void build(vector<int>& nums, int node, int start, int end) {
+        if (start == end) {
+            tree[node] = nums[start];
+        } else {
+            int mid = (start + end) / 2;
+            build(nums, 2 * node + 1, start, mid);
+            build(nums, 2 * node + 2, mid + 1, end);
+            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        }
+    }
+
+    int query(int l, int r) {
+        return queryUtil(0, 0, n - 1, l, r);
+    }
+
+    int queryUtil(int node, int start, int end, int l, int r) {
+        if (r < start || end < l) return 0;
+        if (l <= start && end <= r) return tree[node];
+        int mid = (start + end) / 2;
+        return queryUtil(2 * node + 1, start, mid, l, r) +
+               queryUtil(2 * node + 2, mid + 1, end, l, r);
+    }
+
+    void update(int idx, int val) {
+        updateUtil(0, 0, n - 1, idx, val);
+    }
+
+    void updateUtil(int node, int start, int end, int idx, int val) {
+        if (start == end) {
+            tree[node] = val;
+        } else {
+            int mid = (start + end) / 2;
+            if (idx <= mid)
+                updateUtil(2 * node + 1, start, mid, idx, val);
+            else
+                updateUtil(2 * node + 2, mid + 1, end, idx, val);
+            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        }
+    }
+};
